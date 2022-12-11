e1325bc89a548d96c1887321451f47a0f0882976
aorus 17xe4 <allenliu0517@gmail.com>
2022-12-11 10:07:09 +0800

    test

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+
+function test_btn() 
+{
+    const test = document.getElementById("test");
+    test.classList.toggle("invisible");
+}
+
+function info_btn() 
+{
+    const info = document.getElementById("info");
+    info.classList.toggle("invisible");
+}
+
